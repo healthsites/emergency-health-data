0af63f2fd3e93589352b06a4022d01afdd586980
Janusz Slota <janusz.slota@nixilla.com>
2020-07-02 14:07:40 +0100

    netlify cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mark Herringer`,
+      summary: `Global Healthsites Mapping Project`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `sharehealthdata`,
     },
   },
   plugins: [
@@ -24,6 +24,13 @@ module.exports = {
       options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`,
       },
     },
     {
